41957507e3fe679469354a7746b2f8dff80f318e
Nisarga0104 <159123758+Nisarga0104@users.noreply.github.com>
2024-03-26 21:49:45 +0530

    Add files via upload

---
+++ ayop.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/style5.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mate+SC&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <?php include('navbar.php') ?>
+    <div class="container-fluid bg-image">
+      <div class="row col-12 justify-content-center py-4">
+        <div class="row col-6 py-5 my-5">
+          <div class="btn col-6">
+            <a href="sellp.php"  class="btn btn-outline-light btn-lg aa"><img src="images/sell.png"><br><b>SELL</b></a>
+          </div>
+          <div class="btn col-6">
+            <a href="rentp.php"  class="btn btn-outline-light btn-lg aa"><img src="images/rent.png"><br><b>RENT</b></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include('footer.php') ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </body>
+</html>
