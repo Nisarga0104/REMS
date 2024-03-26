e262742f79f369a5c31a68c159f788d634ad34c6
Nisarga0104 <159123758+Nisarga0104@users.noreply.github.com>
2024-03-26 21:44:04 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,41 @@
+ <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+            <link rel="stylesheet" type="text/css" href="css/style4.css">
+            <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+          <link rel="preconnect" href="https://fonts.gstatic.com">
+          <link href="https://fonts.googleapis.com/css2?family=Mate+SC&display=swap" rel="stylesheet">
+          <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
+
+        </head>
+        <body>
+        <?php include('navbar.php') ?>
+
+        <div class="container-fluid bg-image pt-5">
+        <div class="row col-10 mx-auto pt-5">
+            <div class="col-4 text-center pt-5">
+                <i class="fa fa-home" style="font-size:70px;"></i>
+                <h5>VISIT US<br><strong>Shop-29, Imperial Residency, Behind Agakhan Palace, Kalyani Nagar, Pune, Maharashtra</strong></h5>
+            </div>
+            <div class="col-4 text-center pt-5">
+                <i class="fa fa-phone" style="font-size:70px;"></i>
+                <h5>CALL US<br><strong>+91 (121) 56248</strong></h5>                
+            </div>
+            <div class="col-4 text-center pt-5">
+                <i class="fa fa-envelope" style="font-size:70px;"></i>
+                <h5>MESSAGE US<br><strong>moryarealestate@gmail.com</strong></h5>                
+            </div>            
+        </div>
+        </div>
+
+        <?php include('footer.php') ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        </body>
+        </html>
