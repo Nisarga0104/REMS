fa54e8cb6cc8949a2aab0a7cca938e062b4695a5
Nisarga0104 <159123758+Nisarga0104@users.noreply.github.com>
2024-03-26 16:05:48 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style6.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Mate+SC&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
+    </head>
+    <body style="">
+        <?php include('navbar.php') ?>
+        <div class='about-wrapper' style="background-image: url('./css/bg-image4.jpg')">
+
+            <div class="">
+                <div class=" " >
+                    <div class="">
+                        <h1>ABOUT US</h1>
+                        <h4>There's this notion that to grow a business, you have to be ruthless. But we know there's a better way to grow. One where what's good for the bottom line is also good for customers. We believe businesses can grow with a conscience, and succeed with a soul — and that they can do it with inbound. That's why we've created an ecosystem uniting software, education, and community to help businesses grow better every day.</h4>
+                    </div>
+                <div class="row col-4 justify-content-center">
+                    <!-- <img src="css/about.jpg" height="311px"> -->
+                </div>
+            </div>
+        </div>
+        </div>
+        <?php include('footer.php') ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </body>
+</html>
